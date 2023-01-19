237a646759752865d0c5e8ded2250731dfa8648d
bethapraveen <betha_praveen@yahoo.com>
2023-01-19 23:53:56 +0530

    Dadscool

--- Dadscool/settings.py
+++ Dadscool/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*3$ik$9jxoe$w(rmf_as8fs&j%os^c=)u%-@iyqsagx(fmtdtt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bethapraveen.pythonanywhere.com']
 
 
 # Application definition
