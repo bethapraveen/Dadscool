4d6dfa6f7c0514af432d2ae7c3977a3d5118cee8
bethapraveen <betha_praveen@yahoo.com>
2023-01-20 22:02:11 +0530

    Dadscool

--- QuestionBank/views.py
+++ QuestionBank/views.py
@@ -20,3 +20,64 @@ def index(request):
 
 	return render(request, 'list.html', {'page_obj': page_obj})
 
+def chemistry(request):
+	
+	latest_question_list = list(ChemistryQuestion.objects.order_by('question_viewed_count','-pub_date')[:10])
+	paginator=Paginator(latest_question_list,1)
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+
+	if request.GET.get('question') != None:
+		question = ChemistryQuestion.objects.get(question_id=request.GET.get('question'))
+		#question.update(question_viewed_count, question_viewed_count+1)
+		question.question_viewed_count= question.question_viewed_count+1
+		question.save(update_fields=['question_viewed_count'])
+
+	return render(request, 'list.html', {'page_obj': page_obj})
+
+
+def biology(request):
+	
+	latest_question_list = list(BiologyQuestion.objects.order_by('question_viewed_count','-pub_date')[:10])
+	paginator=Paginator(latest_question_list,1)
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+
+	if request.GET.get('question') != None:
+		question = BiologyQuestion.objects.get(question_id=request.GET.get('question'))
+		#question.update(question_viewed_count, question_viewed_count+1)
+		question.question_viewed_count= question.question_viewed_count+1
+		question.save(update_fields=['question_viewed_count'])
+
+	return render(request, 'list.html', {'page_obj': page_obj})
+
+def maths(request):
+	
+	latest_question_list = list(MathsQuestion.objects.order_by('question_viewed_count','-pub_date')[:10])
+	paginator=Paginator(latest_question_list,1)
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+
+	if request.GET.get('question') != None:
+		question = MathsQuestion.objects.get(question_id=request.GET.get('question'))
+		#question.update(question_viewed_count, question_viewed_count+1)
+		question.question_viewed_count= question.question_viewed_count+1
+		question.save(update_fields=['question_viewed_count'])
+
+	return render(request, 'list.html', {'page_obj': page_obj})
+
+def statistics(request):
+	
+	latest_question_list = list(StatisticsQuestion.objects.order_by('question_viewed_count','-pub_date')[:10])
+	paginator=Paginator(latest_question_list,1)
+	page_number = request.GET.get('page')
+	page_obj = paginator.get_page(page_number)
+
+	if request.GET.get('question') != None:
+		question = StatisticsQuestion.objects.get(question_id=request.GET.get('question'))
+		#question.update(question_viewed_count, question_viewed_count+1)
+		question.question_viewed_count= question.question_viewed_count+1
+		question.save(update_fields=['question_viewed_count'])
+
+	return render(request, 'list.html', {'page_obj': page_obj})
+
