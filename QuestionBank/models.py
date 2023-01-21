a57be85554b070528fbe9415b7bb22f59b236f29
bethapraveen <betha_praveen@yahoo.com>
2023-01-21 19:39:58 +0530

    Dadscool

--- QuestionBank/models.py
+++ QuestionBank/models.py
@@ -9,7 +9,7 @@ class PhysicsQuestion(models.Model):
 	question_text = models.ImageField(upload_to='PhysicsQuestions')
 	question_viewed_count=models.IntegerField(default='0')	
 	question_id=models.AutoField(primary_key=True)
-	pub_date = models.DateTimeField('date published',default=timezone.now())
+	pub_date = models.DateTimeField('date published',default=timezone.now)
 	question_answer = models.ImageField(upload_to='PhysicsQuestions')
 	def img_preview(self): #new
 		return mark_safe(f'<img src = "{self.question_text.url}" />')
@@ -20,7 +20,7 @@ class ChemistryQuestion(models.Model):
 	question_text = models.ImageField(upload_to='ChemistryQuestions')
 	question_viewed_count=models.IntegerField(default='0')	
 	question_id=models.AutoField(primary_key=True)
-	pub_date = models.DateTimeField('date published',default=timezone.now())
+	pub_date = models.DateTimeField('date published',default=timezone.now)
 	question_answer = models.ImageField(upload_to='ChemistryQuestions')
 	def img_preview(self): #new
 		return mark_safe(f'<img src = "{self.question_text.url}" />')
@@ -31,7 +31,7 @@ class BiologyQuestion(models.Model):
 	question_text = models.ImageField(upload_to='BiologyQuestion')
 	question_viewed_count=models.IntegerField(default='0')	
 	question_id=models.AutoField(primary_key=True)
-	pub_date = models.DateTimeField('date published')
+	pub_date = models.DateTimeField('date published',default=timezone.now)
 	question_answer = models.ImageField(upload_to='BiologyQuestion')
 	def img_preview(self): #new
 		return mark_safe(f'<img src = "{self.question_text.url}" />')
@@ -42,7 +42,7 @@ class MathsQuestion(models.Model):
 	question_text = models.ImageField(upload_to='MathsQuestion')
 	question_viewed_count=models.IntegerField(default='0')	
 	question_id=models.AutoField(primary_key=True)
-	pub_date = models.DateTimeField('date published')
+	pub_date = models.DateTimeField('date published',default=timezone.now)
 	question_answer = models.ImageField(upload_to='MathsQuestion')
 	def img_preview(self): #new
 		return mark_safe(f'<img src = "{self.question_text.url}" />')
@@ -53,7 +53,7 @@ class StatisticsQuestion(models.Model):
 	question_text = models.ImageField(upload_to='StatisticsQuestion')
 	question_viewed_count=models.IntegerField(default='0')	
 	question_id=models.AutoField(primary_key=True)
-	pub_date = models.DateTimeField('date published')
+	pub_date = models.DateTimeField('date published',default=timezone.now)
 	question_answer = models.ImageField(upload_to='StatisticsQuestion')
 	def img_preview(self): #new
 		return mark_safe(f'<img src = "{self.question_text.url}" />')
