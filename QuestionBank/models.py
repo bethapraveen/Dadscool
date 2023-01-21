9f3e4e7575da695b78d31c88daadebf06f360f0c
bethapraveen <betha_praveen@yahoo.com>
2023-01-21 19:27:57 +0530

    Dadscool

--- QuestionBank/models.py
+++ QuestionBank/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from django.utils.html import mark_safe
 
@@ -8,7 +9,7 @@ class PhysicsQuestion(models.Model):
 	question_text = models.ImageField(upload_to='PhysicsQuestions')
 	question_viewed_count=models.IntegerField(default='0')	
 	question_id=models.AutoField(primary_key=True)
-	pub_date = models.DateTimeField('date published')
+	pub_date = models.DateTimeField('date published',default=timezone.now())
 	question_answer = models.ImageField(upload_to='PhysicsQuestions')
 	def img_preview(self): #new
 		return mark_safe(f'<img src = "{self.question_text.url}" />')
@@ -19,7 +20,7 @@ class ChemistryQuestion(models.Model):
 	question_text = models.ImageField(upload_to='ChemistryQuestions')
 	question_viewed_count=models.IntegerField(default='0')	
 	question_id=models.AutoField(primary_key=True)
-	pub_date = models.DateTimeField('date published')
+	pub_date = models.DateTimeField('date published',default=timezone.now())
 	question_answer = models.ImageField(upload_to='ChemistryQuestions')
 	def img_preview(self): #new
 		return mark_safe(f'<img src = "{self.question_text.url}" />')
